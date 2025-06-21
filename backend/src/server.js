f1b806d02e023bd376b0a3c6a1c009c8fe2ebaf8
Rakesh Singh <rakeshsingh432165@gmail.com>
2025-06-22 03:01:30 +0530

    refactor: cors error tackled

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,10 +16,6 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 app.use(express.json());
-app.use(cookieParser());
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/chat", chatRoutes);
 
 app.use(
     cors({
@@ -28,13 +24,10 @@ app.use(
     })
 );
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-    });
-}
-
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
